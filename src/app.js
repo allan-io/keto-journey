4b320e20ea3ba10dc756fea18bbe97e49eafae98
Allan <allanoliveira.dev@gmail.com>
2021-01-05 23:55:17 -0500

    set up paths and started server

--- src/app.js
+++ src/app.js
@@ -3,3 +3,11 @@ const express = require('express')
 
 const app = express()
 
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates')
+
+app.use(express.static(publicDirectoryPath))
+
+app.listen(4000, () => {
+    console.log('Server is running on port 4000')
+})
