c0223a38f6c3c92268606c3f9c563ece6245943d
Allan <allanoliveira.dev@gmail.com>
2021-01-06 00:24:38 -0500

    created routes and hbs files to be rendered

--- src/app.js
+++ src/app.js
@@ -6,7 +6,36 @@ const app = express()
 const publicDirectoryPath = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates')
 
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+
 app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Keto Journey'
+    })
+})
+app.get('/blog', (req, res) => {
+    res.render('index', {
+        title: 'Blog'
+    })
+})
+app.get('/collections', (req, res) => {
+    res.render('collections', {
+        title: 'Collections'
+    })
+})
+app.get('/contact', (req, res) => {
+    res.render('contact', {
+        title: 'Contact'
+    })
+})
+app.get('/recipes', (req, res) => {
+    res.render('recipes', {
+        title: 'Recipes'
+    })
+})
 
 app.listen(4000, () => {
     console.log('Server is running on port 4000')
