368242c3a0c64f4f9e8b8ccd67fc4c484f80e131
Allan <allanoliveira.dev@gmail.com>
2021-01-06 00:47:57 -0500

    configured partials and created header and footer partials files

--- src/app.js
+++ src/app.js
@@ -1,13 +1,16 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
 
 const publicDirectoryPath = path.join(__dirname, '../public')
-const viewsPath = path.join(__dirname, '../templates')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
 
 app.set('view engine', 'hbs')
 app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
 
 app.use(express.static(publicDirectoryPath))
 
@@ -17,7 +20,7 @@ app.get('', (req, res) => {
     })
 })
 app.get('/blog', (req, res) => {
-    res.render('index', {
+    res.render('blog', {
         title: 'Blog'
     })
 })
